8b5dd3a1d3b627a3ee2f359194d6765de45f71d6
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-09 11:43:49 -0600

    added comments, finished on-click functionality

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,48 +1,87 @@
 $(document).ready(function () {
-    // Predefined array of items
+    // Array to hold topics entered by user
     var topics = ["cars", "guitars", "trucks"];
 
+    // Display buttons onto the screen
     displayButtons(topics);
 
+    // Function that will run when user submits a new topic
     $("#add-gif").on("click", function (e) {
         e.preventDefault();
 
+        // Get the input value from the input box
         var newItem = $("#newItem").val().trim();
+
+        // Alert user if there is no text entered
         if (!newItem) {
             alert("No input detected");
         }
+        // Otherwise, add to array
         else {
             topics.push(newItem);
         }
 
+        // Display buttons onto the screen
         displayButtons(topics);
     });
 
+    // Request gif images and show to screen
     function displayGif(gif) {
+
+        // Clear any pre-existing images on the screen
         $("#gifs").empty();
 
-        var inputText = $(this).data("value");
+        var apiKey = "fNct1kLAqG5QM1YdMbjB8WkWmUXnFb5Y"; //Generated API key from GIPHY
+        var inputText = $(this).data("value"); // Get the text from input box
+        var param1 = "&api_key="; // Parameter used to inlcude API key
+        var param2 = "&limit=10"; // Paramater used to limit number of results returned
+        var giphyURL = "http://api.giphy.com/v1/gifs/search?q="; // URL for querying GIPHY's APIs
 
-        var apiKey = "&api_key=fNct1kLAqG5QM1YdMbjB8WkWmUXnFb5Y";
-        var param = "&limit=10";
-        var giphyURL = "http://api.giphy.com/v1/gifs/search?q=";
+        // Set the query URL to include URL, API key, text value and all necessary parameters
+        var queryURL = giphyURL + inputText + param1 + apiKey + param2;
 
-        var queryURL = giphyURL + inputText + apiKey + param;
-
+        // Run query using AJAX GET method
         $.ajax({
             url: queryURL,
             method: "Get"
+
+            // When request has been received, execute the following Promise function
         }).then(function (response) {
             var gif = response.data;
+
+            // Loop through the results and display gif image for each item
             for (var i = 0; i < gif.length; i++) {
-                $("#gifs").append("<img src='" + gif[i].images.fixed_width.url + "'/>");
 
+                var still = gif[i].images.fixed_width_still.url;
+                var animate = gif[i].images.fixed_width.url;
+
+                var newGif = $("<img>").attr("src", still)
+                    .attr("class", "giphy")
+                    .attr("data-state", "still")
+                    .attr("data-still", still)
+                    .attr("data-animate", animate);
+
+                $("#gifs").append(newGif);
             }
+
+
+            $(".giphy").on("click", function () {
+                var state = $(this).attr("data-state");
+
+                if (state === "still") {
+                    $(this).attr("src", $(this).data("animate"));
+                    state = $(this).attr("data-state", "animate");
+                }
+
+                if (state === "animate") {
+                    $(this).attr("src", $(this).data("still"));
+                    state = $(this).attr("data-state", "still");
+                }
+            });
         });
-
-
     }
 
+    // Displays the buttons on the screen
     function displayButtons(newArray) {
         $("#display-buttons").empty();
         for (var i = 0; i < newArray.length; i++) {
